50d65e0fb6c7f889686cefd4b3b2c64f91093a7d
Haasith Sanka <haasithsanka@haasiths-macbook-pro.local>
2017-01-17 23:43:34 -0800

    Initial Commit

---
+++ samp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  samp
+//
+//  Created by Haasith Sanka on 1/17/17.
+//  Copyright © 2017 Haasith Sanka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
